d8a72325a036151324e85404ce6f4ee648e239e1
Gareth Jones <gareth.jones@shinetech.com>
2016-09-07 15:20:02 +1000

    bumped version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'pow-v15';
+var cacheName = 'pow-v16';
 var filesToCache = [
   "bower_components/bootstrap/dist/css/bootstrap.min.css",
   "bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
